4d9352bef6a631cc473e90bb3c0ad4f76e3d75c1
wag192625 <wag192625@gmail.com>
2022-06-13 12:08:37 +0900

    자바 생성

---
+++ studyJava/hello.java
@@ -0,0 +1,7 @@
+// package studyJava;
+
+class hello {
+    public static void main(String[] arg) {
+        System.out.println("output");
+    }
+}
---
+++ 수업/5주차/hello.java
@@ -0,0 +1,8 @@
+// package 작성
+//파일명과 클래스명 동일
+class main {
+    public static void main(String[] abcd) {
+        System.out.println("Hello world!");
+        // println다음 () 에 ""를 붙이면 자동으로 x:가 붙는다.
+    }
+}
