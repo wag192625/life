c27848d34945ba31eb759904b8167ee3c455c3ec
wag192625 <wag192625@gmail.com>
2022-06-16 18:54:49 +0900

    sosu

--- studyJava/소수 만들기/소수 만들기.java
+++ studyJava/소수 만들기/소수 만들기.java
@@ -1,4 +1,4 @@
-import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+import java.util;
 
 class Solution {
     public static void main(String[] arg) {
@@ -7,24 +7,26 @@ class Solution {
     }
     public int solution(int[] nums) {
     int[] numbers = {1,2,3,4,5,6,7,8};
-        int answer = 0;
-        // int[] array = numbers;
-        // ArrayList<Integer> num = new ArrayList<Integer>();
+    int n = numbers;
+    int answer = 0;
+    boolean[] isPrime = new boolean[n+1];
+    Arrays.fill(isPrime , true);
+
+        isPrime [0] = isPrime [1] = false;
         for(int i = 0; i<numbers.length-2; i++) {
             for(int j = i+1; j<numbers.length-1; j++) {
                 for(int k = j+1; k<numbers.length; k++){
                     int sum = (numbers[i] + numbers[j] + numbers[k]);
-                    // System.out.println(sum);
                     if(sum % 2 == 0){ // 2로 나누어 떨어지면 (약수도 포함됨)
-                        if(1)// 약수 제거 문제
+                        if(sum )// 약수 제거 문제
                         answer++; //결괏값 0나옴
                     }
                 }
             }
         }
         return answer;
-        }
     }
+}
     //if(소수이면 );
     // public boolean sum(int n){
     //     if(n == 1) return false;
