55b0c28c8f0d645238558dbbbab8b007b7fecfc0
wag192625 <wag192625@gmail.com>
2022-06-17 13:46:39 +0900

    123

--- studyJava/소수 만들기/소수 만들기.java
+++ studyJava/소수 만들기/소수 만들기.java
@@ -1,27 +1,39 @@
 import java.util.*;
-
 class Solution {
-    public static void main(String[] arg) {
-        // int[] num = {1,2,3,4,5,6};
-        // solution(num)
-    }
+
     public int solution(int[] nums) {
-    int[] numbers = {1,2,3,4,5,6,7,8};
-    // int n = numbers;
+    // int[] nums = {1,2,3,4,5,6,7,8};
     int answer = 0;
-    // boolean[] isPrime = new boolean[n+1];
-    // Arrays.fill(isPrime , true);
-    // isPrime [0] = isPrime [1] = false;
-        for(int i = 0; i<numbers.length-2; i++) {
-            for(int j = i+1; j<numbers.length-1; j++) {
-                for(int k = j+1; k<numbers.length; k++){
-                    int sum = (numbers[i] + numbers[j] + numbers[k]);
-                        for(int z = 2; z<sum; z++){
-                            if(sum % z == 0)
-                                answer++;}
+
+        for(int i = 0; i<nums.length-2; i++) {
+            for(int j = i+1; j<nums.length-1; j++) {
+                for(int k = j+1; k<nums.length; k++) {
+                    int sum = (nums[i] + nums[j] + nums[k]);
+                    //3개의 수를 더해야 해서 for문 3개 사용
+                    // System.out.println(sum);
+                        for(int l=2; l<sum; l++){
+                            //2로 먼저 나누기 위해 설정
+                            //왠지 모르겠지만 2가 아니라 3으로 해도 모두 통과
+                            if(sum%l == 0 ){
+                                break;//sum의 숫자와 
+                            }
+                            if(l==sum-1){
+                            //
+                                answer++;
+                            }
+                        }
+                    //     if(b(sum)){
+                    //         answer++;
+                    // }
                 }
             }
         }
         return answer;
     }
+    // public static boolean b(int num){
+    //     for(int i = 2; i < num; i++){
+    //         if(num % i== 0)return false;
+    //     }
+    //     return true;
+    // }
 }
---
+++ 시험/답안지3.js
@@ -0,0 +1,10 @@
+
+function solution() {
+    let ans
+    let arr1 = [[1,2],[2,3]] ;
+    let arr2 = [[3,4],[5,6]] ;
+    let sum = 0;
+    return sum;
+    }
+    let result=printArr();
+    document.write(result);
