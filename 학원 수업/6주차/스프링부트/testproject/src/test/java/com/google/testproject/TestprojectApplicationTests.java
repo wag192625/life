9e2a8891fce1794e3d16851d29b0a58ba53f3c68
wag192625 <wag192625@gmail.com>
2022-06-20 09:33:27 +0900

    123

---
+++ 학원 수업/6주차/스프링부트/testproject/src/test/java/com/google/testproject/TestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
