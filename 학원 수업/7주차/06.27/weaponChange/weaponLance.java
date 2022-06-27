376ff59fce51d9133cf05febda86ded499196c76
wag192625 <wag192625@gmail.com>
2022-06-27 11:11:09 +0900

    123

---
+++ 학원 수업/7주차/06.27/weaponChange/weaponLance.java
@@ -0,0 +1,24 @@
+package weaponChange;
+
+public class weaponLance extends weapon1 {
+    weaponLance() {
+        this.name = "랜스";
+        this.ATTRange = 2;
+        this.ChangeTime = 3;
+
+
+    }
+    @Override
+    public void attAction() {
+        System.out.println("무기"+this.name+"을 장착하였습니다.");
+        System.out.println("무기의 공격 범위는 "+this.ATTRange+" 입니다.");
+        System.out.println("무기의 교체 시간은 "+this.ChangeTime+" 입니다.");
+    }
+
+    @Override
+    public void destrution() {
+        if(this.name == "랜스") {
+            
+        }
+    }
+}
