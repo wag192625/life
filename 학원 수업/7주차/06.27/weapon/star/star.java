62de00a78a177cbee4b33a464b5903bf2b4ecc9f
wag192625 <wag192625@gmail.com>
2022-06-28 22:14:55 +0900

    123

--- 학원 수업/7주차/06.27/weapon/star/star.java
+++ 학원 수업/7주차/06.27/weapon/star/star.java
@@ -1,27 +1,41 @@
+package star;
 import java.util.*;
 
 public class star {
 
     public static void main(String[] arg) {
-        // int x= 6;
-        int y= 7;
-        // int z= 3;
-        for(int i=0; i<y; i++){
-            
-            
-            System.out.println();
+
+        int[][] arr1 = {{1,2},{3,4},{5,6}}; //int[3][2] 
+        int[][] arr2 = {{3,4},{5,6},{7,8}};
+        int[][] answer = new int[3][2];
+        for(int i = 0; i<arr1.length; i++) {
+            for(int j = 0; j<arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+                System.out.print("["+answer[i][j]+"]");
+                }
+            }
         }
     }
-    
-    
-    
-    
-        //직각삼각형 람다식
+
+        // int x= 6;
+        // int y= 7;
+        // int z= 3;
+    //     for(int i=0; i<y; i++){
+    //          for(int k=y; k>i; k--) {
+    //              System.out.print("*");
+    //             }
+    //         System.out.println();
+    //         }
+    //     }
+    // }
+
+        // 직각삼각형 람다식
         // for(int i=0; i<y; i++) {
         //     Arrays.asList(new String[i]).stream().forEach(_row -> System.out.print("*"));
         //     System.out.println("");
+        //         }
+        //     }
         // }
-
         // 사각형
         // for(int i=0; i<=y; i++) {
         //     for(int j=0; j<x; j++) {
@@ -63,4 +77,3 @@ public class star {
             //     }
 
     // }
-}
