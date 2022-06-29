8b6b407ca92519b34eb7bdb452c389bc80d8d901
wag192625 <wag192625@gmail.com>
2022-06-29 18:48:09 +0900

    123

--- 학원 수업/7주차/06.27/weapon/star/star.java
+++ 학원 수업/7주차/06.27/weapon/star/star.java
@@ -1,25 +1,28 @@
 package star;
+
 import java.util.*;
 
 public class star {
 
     public static void main(String[] arg) {
+        
+        
+        int x= 6;
+        int y= 7;
+        int z= 3;
 
+        // 원
+        // for2 if1
+
+        //정삼각형 2
+        for(int i=0; i<y; i++){
+            for(int j=y; j>i; j++){
+     
+            }
+            System.out.println();
+           }
+        }
     }
-}
-
-        // int x= 6;
-        // int y= 7;
-        // int z= 3;
-    //     for(int i=0; i<y; i++){
-    //          for(int k=y; k>i; k--) {
-    //              System.out.print("*");
-    //             }
-    //         System.out.println();
-    //         }
-    //     }
-    // }
-
         // 직각삼각형 람다식
         // for(int i=0; i<y; i++) {
         //     Arrays.asList(new String[i]).stream().forEach(_row -> System.out.print("*"));
@@ -27,14 +30,17 @@ public class star {
         //         }
         //     }
         // }
+
+
         // 사각형
         // for(int i=0; i<=y; i++) {
         //     for(int j=0; j<x; j++) {
-            //         System.out.print( "*");
+        //         System.out.print( "*");
         //     }
         // System.out.println();
         // }
-        //
+        
+
         // // 직각삼각형
         // for(int i=0; i<=y; i++) {
         //     for(int j=0; j<i; j++) {
@@ -43,12 +49,78 @@ public class star {
             // System.out.println();
             // }
 
+
         // // 역삼각형
         // for(int i=0; i<y; i++){
         //      for(int k=y; k>i; k--) {
         //          System.out.print("0");
         //      }}
-            
+        
+        //역삼각형2
+
+        // for(int i=0; i<y; i++){
+        //     for(int j=0; j<y-i;j++){
+        //         System.out.print("*");
+        //         }
+        //     System.out.println();
+        //    }
+        
+        // 정삼각형
+        // for(int i=0; i<y; i++){
+        //     for(int j=y; j>i; j--) {
+        //         System.out.print(" ");
+        //        }
+        //    for(int k=0; k<i+1; k++) {
+        //        System.out.print("1");
+        //        }
+        //    for(int l=0; l<i; l++) {
+        //        System.out.print("2");
+        //        }
+        //    System.out.println();
+        //    }
+
+        //정삼각형 2
+        // for(int i=0; i<y; i++){
+        //     for(int j=y; j>i; j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=0; k<i+1; k++){
+        //         if(k<i){
+        //             System.out.print("+");
+        //         }
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //    }
+        // }
+
+        //마름모
+        // for(int i=0; i<y; i++){
+        //     for(int j=y; j>i; j--) {
+        //         System.out.print(" ");
+        //         }
+        //     for(int k=0; k<i; k++) {
+        //         System.out.print("1");
+        //         }
+        //     for(int l=0; l<i-1; l++) {
+        //         System.out.print("2");
+        //         }
+        //     System.out.println();
+        //    }
+        // for(int i=0; i<y; i++){
+        //     for(int j=0; j<i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=y; k>i; k--){
+        //         System.out.print("3");
+        //     }
+        //     for(int l=y; l>i+1; l--){
+        //         System.out.print("4");
+        //     }
+        //     System.out.println();
+        // }
+
+
             // //뭔가
     //     for(int i=0; i<=y; i++) {
     //         for(int j=0; j<i+1; j++) {
