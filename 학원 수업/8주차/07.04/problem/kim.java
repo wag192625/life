49f6043302f36f80aadfe043ad38125d341b48a9
wag192625 <wag192625@gmail.com>
2022-07-05 16:20:03 +0900

    1123

--- 학원 수업/8주차/07.04/problem/kim.java
+++ 학원 수업/8주차/07.04/problem/kim.java
@@ -2,18 +2,17 @@ package problem;
 
 import java.util.concurrent.TimeUnit;
 
-public class kim implements time {
-    int[] kim = { 11, 20, 44, 13 };
-
+public class Kim implements time {
+    int[] kim = { 39, 12, 63, 44 };
     public void loding() {
         try {
             for (int i = 0; i <= 200; i++) {
                 System.out.println("Sleep " + i);
-                if (i % 5 == 0) {
+                if (i % 3 == 0) {
                     System.out.println("3초 김" + kim);
-                }     
+                }
                 TimeUnit.SECONDS.sleep(1);
-            }     
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
